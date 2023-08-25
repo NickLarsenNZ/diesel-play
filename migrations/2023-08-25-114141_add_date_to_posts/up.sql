91b32c773e33d9ecb9d9ddf350eda11779cdcfd4
NickLarsenNZ <nick@aptiv.co.nz>
2023-08-25 13:45:33 +0200

    diesel migration generate add_date_to_posts

---
+++ migrations/2023-08-25-114141_add_date_to_posts/up.sql
@@ -0,0 +1,2 @@
+-- Your SQL goes here
+ALTER TABLE posts ADD COLUMN published_date TIMESTAMP;
--- src/schema.rs
+++ src/schema.rs
@@ -6,5 +6,6 @@ diesel::table! {
         title -> Varchar,
         body -> Text,
         published -> Bool,
+        published_date -> Nullable<Timestamp>,
     }
 }
